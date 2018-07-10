0c5414f054d563fbfb5e2bd03b2528fa78273f7c
Kaiser De Kam <kaiserdekam@kaisers-mac-pro.local>
2018-07-10 17:28:00 -0400

    Initial Commit

---
+++ scheduleapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scheduleapp
+//
+//  Created by Kaiser De Kam on 7/10/18.
+//  Copyright © 2018 Kaiser De Kam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
