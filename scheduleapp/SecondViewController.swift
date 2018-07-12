ef26f4507e0fb812aa19e6b0eb24235a16062396
Kaiser De Kam <kaiserddekam@gmail.com>
2018-07-12 19:22:38 -0400

    Start Time Available
    Added ability to select the start time for an event.

--- scheduleapp/SecondViewController.swift
+++ scheduleapp/SecondViewController.swift
@@ -15,23 +15,64 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var inputTwo: UITextField!
     
     
+    //TIME PICKER
+    @IBOutlet weak var timeInputStart: UITextField! //datePickerTF
+    let timePicker = UIDatePicker()
+
+    func createDatePicker(){
+        //format the display of timepicker
+        timePicker.datePickerMode = .time
+        
+            //assign Date picker to our textfield
+        timeInputStart.inputView = timePicker
+        
+        // create a toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //add a done button on the toolbar
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
+        toolbar.setItems([doneButton], animated: true)
+        timeInputStart.inputAccessoryView = toolbar
+        
+        }
+    @objc func doneClicked() {
+        //format the date display in textfield
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .short
+        timeFormatter.dateStyle = .none
+        timeInputStart.text = timeFormatter.string(from: timePicker.date)
+
+        //End Picker window
+        self.view.endEditing(true)
+        print(timeInputStart.text!)
+
+    }
+    
+    
+    
     //Add to data
     @IBAction func addItem(_ sender: Any) {
         let inputSourceOne = inputOne.text
         let inputSourceTwo = inputTwo.text
-        if (inputSourceOne != "" || inputSourceTwo != "") {
-            let newItem = myData(firstRowLabel: inputSourceOne!, secondRowLabel: inputSourceTwo!)
+        let inputTimeStart = timeInputStart.text
+//        let startTime =
+//        let stopTime =
+        
+        if (inputSourceOne != "" || inputSourceTwo != "" || inputTimeStart != "") {
+            let newItem = myData(firstRowLabel: inputSourceOne!, secondRowLabel: inputSourceTwo!, startTimeLabel: inputTimeStart!)
             tableData.append(newItem)
             print(tableData)
             inputOne.text = ""
             inputTwo.text = ""
-            
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createDatePicker()
+
     }
     
     override func didReceiveMemoryWarning() {
